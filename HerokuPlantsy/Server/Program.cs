15c4ffc01556dd6ae208251b155a4cc9ef96e659
stian.johannessen-tvedt@intility.no <stian.johannessen-tvedt@intility.no>
2021-06-28 21:51:53 +0200

    all fix
    Signed-off-by: stian.johannessen-tvedt@intility.no <stian.johannessen-tvedt@intility.no>

--- HerokuPlantsy/Server/Program.cs
+++ HerokuPlantsy/Server/Program.cs
@@ -21,6 +21,7 @@ namespace HerokuPlantsy.Server
 				.ConfigureWebHostDefaults(webBuilder =>
 				{
 					webBuilder.UseStartup<Startup>();
+					webBuilder.UseUrls("http://*:" + Environment.GetEnvironmentVariable("PORT"));
 				});
 	}
 }
