674f7db8449a6a351534af25d92d568f07838227
stian.johannessen-tvedt@intility.no <stian.johannessen-tvedt@intility.no>
2021-07-11 19:27:40 +0200

    clr mess

--- HerokuPlantsy/Client/Program.cs
+++ HerokuPlantsy/Client/Program.cs
@@ -1,14 +1,10 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using HerokuPlantsy.Client.Shared;
+
 
 namespace HerokuPlantsy.Client
 {
